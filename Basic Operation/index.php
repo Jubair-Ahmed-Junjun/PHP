db0bdda1e0fde3dd7975988abca08f0495036bdc
Jubair Ahmed Junjun <51238714+junjuncse@users.noreply.github.com>
2020-06-30 11:01:16 -0700

    basic operation

---
+++ Basic Operation/index.php
@@ -0,0 +1,56 @@
+<?php
+  //echo "Hello World";
+ // print "Lol";
+
+
+                                         /* Write HTML in PHP*/
+
+
+//echo "<html>";
+//echo "<head>";
+//echo "<title>";
+//echo "Home";
+//echo "</title>";
+//echo "</head>";
+//echo "<body>";
+//echo "<h1>";
+//echo "This a hading.";
+//echo "</h1>";
+//echo "</body>";
+//echo "</html>";
+
+
+?>
+                             <!--   Write PHP in HTML  -->
+<!--<html>-->
+<!--<head>-->
+<!--    <title>Hom</title>-->
+<!--</head>-->
+<!--<h1>--><?php //echo "This is Hading"?><!--</h1>-->
+<!--<p>--><?php //echo "This is paragraph"?><!--</p>-->
+<!--<input type="text" value="--><?php //echo "Hello World"?><!--">-->
+<!--</html>-->
+
+
+
+
+
+
+<?php
+
+
+                                        // 3 major rules for creating of a Variable
+
+                                           /*
+                                            *Start with $ sign
+                                            * A-Z,a-z,0-9,_
+                                            * No number in first
+                                           */
+                                        // 3 standard rules for creating of a variable
+
+                                           /*
+                                              *small latter
+                                              *meaning full
+                                              *readable
+                                            */
+?>
