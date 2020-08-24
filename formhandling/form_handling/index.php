757eba867813042bf210f4a62026339b9ec08be6
Jubair Ahmed Junjun <51238714+Jubair-Ahmed-Junjun@users.noreply.github.com>
2020-08-24 19:18:17 +0600

    Add files via upload
    php form handling

---
+++ formhandling/form_handling/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>From Handling</title>
+</head>
+<body>
+    <form action="welcome.php" method="POST">
+     Email : <input type="email" name="email" placeholder="enter your email"><br>
+     Password : <input type="password" name="password" placeholder="enter your password"><br>
+     <input type="submit" name="btn" value="submit">
+    </form>
+</body>
+</html>
---
+++ formhandling/form_handling/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>welcome</title>
+</head>
+<body>
+    Your email : <?php echo $_POST['email'] ; ?>
+    Password : <?php echo $_POST['password']; ?>
+</body>
+</html>
